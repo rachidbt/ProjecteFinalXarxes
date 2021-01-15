538495f4c5fd69acc4dafb64eabf3c72b67fc3fe
antoni8 <72940587+antoni8@users.noreply.github.com>
2021-01-15 19:51:28 +0100

    Update script_mail.sh

--- script_mail.sh
+++ script_mail.sh
@@ -4,5 +4,5 @@ data=`date +%s`
 usuari=$user$data
 
 useradd $usuari -m
-sed -i 's/!/$passwd/g' /etc/shadow
-sed -i 's/mydestination=/mydestination= $user.com, /g' /etc/postfix/main.cf.prova
+sed -i "s/!/$passwd/g" /etc/shadow
+sed -i "s/mydestination=/mydestination= $user.com, /g" /etc/postfix/main.cf
